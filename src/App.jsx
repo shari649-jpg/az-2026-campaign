f72f7e406e797ee2d2cb631270b03791cf622057
shari649-jpg <shari649@gmail.com>
2026-05-25 16:57:23 -0700

    Initialize App component with routing setup
    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppShell from "./components/AppShell";
+import HomePage from "./components/HomePage";
+import ResearchPage from "./tools/research/ResearchPage";
+import MessagingPage from "./tools/messaging/MessagingPage";
+import RebuttalPage from "./tools/rebuttal/RebuttalPage";
+import RapidResponsePage from "./tools/rapid-response/RapidResponsePage";
+import ResourcesPage from "./tools/resources/ResourcesPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppShell />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/research" element={<ResearchPage />} />
+          <Route path="/messaging" element={<MessagingPage />} />
+          <Route path="/rebuttal" element={<RebuttalPage />} />
+          <Route path="/rapid-response" element={<RapidResponsePage />} />
+          <Route path="/resources" element={<ResourcesPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
